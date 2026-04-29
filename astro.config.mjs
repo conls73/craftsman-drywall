428a57b47ce2e9efecfbc813a832947bb99d7974
GPTQuills <gptquillen@gmail.com>
2026-04-29 15:11:36 -0700

    Disabled Astro dev toolbar and removed generator meta tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://craftsmandrywallllc.com',
   integrations: [tailwind(), sitemap()],
+  devToolbar: {
+    enabled: false
+  }
 });
